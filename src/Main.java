558516eb4a9d7eaccd82e9a34f22bec888ba2aa7
Johann <johann.geisser.aguilar@gmail.com>
2022-07-25 22:05:16 +0200

    Adding loop functionality to menu

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,39 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
 
-        System.out.println("Enter action (add, remove, edit, count, list, exit):");
-        String action = scanner.next();
-        switch (action) {
-            case "add":
-                System.out.println("Adding");
-                break;
-            case "remove":
-                System.out.println("Removing");
-                break;
-            case "edit":
-                System.out.println("Editing");
-                break;
-            case "count":
-                System.out.println("Counting");
-                break;
-            case "list":
-                System.out.println("Listing");
-                break;
-            case "exit":
-                System.out.println("Exiting");
-            default:
-                System.out.println("Choose a valid option");
-        }
+        do {
+            System.out.println("Enter action (add, remove, edit, count, list, exit):");
+            String action = scanner.next();
+            switch (action) {
+                case "add":
+                    System.out.println("Adding");
+                    break;
+                case "remove":
+                    System.out.println("Removing");
+                    break;
+                case "edit":
+                    System.out.println("Editing");
+                    break;
+                case "count":
+                    System.out.println("Counting");
+                    break;
+                case "list":
+                    System.out.println("Listing");
+                    break;
+                case "exit":
+                    System.out.println("Exiting");
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Choose a valid option");
+            }
+        } while (exit == false);
+
+
+
+
 
 //        System.out.println("Enter the name of the person:");
 //        String name = scanner.next();
