692e7bdf30e4a8f4da54ef9776765f89c2d320a2
Johann <johann.geisser.aguilar@gmail.com>
2022-07-26 21:41:21 +0200

    Modifying edit case so it uses PB class & implementing editRecord method in PB class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean exit = false;
-
-        //Probablemente sea bueno crear una clase PhoneBook que me guarde la lista de los objetos de la clase Contact, en vez de crear la lista en la clase main
-        //ArrayList<Contact> records = new ArrayList<Contact>();
         PhoneBook phoneBook = new PhoneBook();
+        boolean exit = false;
 
         do {
             System.out.println("Enter action (add, remove, edit, count, list, exit):");
@@ -37,41 +33,16 @@ public class Main {
                         phoneBook.removeRecords(recordToRemove);
                     }
                     break;
-//                case "edit":
-//                    if (records.size() == 0) {
-//                        System.out.println("No records to edit!");
-//                    } else {
-//                        for (int i = 0; i < records.size(); i++) { //Al mostrar la lista aquí por medio de la misma implementación que en list, me estoy repitiendo, por lo que esta sección se puede optimizar
-//                            System.out.println(i + 1 + ". " + records.get(i));
-//                        }
-//                        System.out.println("Select a record:");
-//                        int record = scanner.nextInt();
-//                        System.out.println("Select a field (name, surname, number):");
-//                        String field = scanner.next();
-//                        switch (field) {
-//                            case "name":
-//                                System.out.println("Enter the name:");
-//                                String newName = scanner.next();
-//                                records.get(record - 1).setName(newName);
-//                                System.out.println("The record updated!");
-//                                break;
-//                            case "surname":
-//                                System.out.println("Enter the surname:");
-//                                String newSurname = scanner.next();
-//                                records.get(record - 1).setSurname(newSurname);
-//                                System.out.println("The record updated!");
-//                                break;
-//                            case "number":
-//                                System.out.println("Enter the number:");
-//                                String newNumber = scanner.next();
-//                                records.get(record - 1).setPhoneNumber(newNumber);
-//                                System.out.println("The record updated!");
-//                                break;
-//                            default:
-//                                System.out.println("No valid option");
-//                        }
-//                    }
-//                    break;
+                case "edit":
+                    if (phoneBook.countRecords() == 0) {
+                        System.out.println("No records to edit!");
+                    } else {
+                        phoneBook.displayRecords();
+                        System.out.println("Select a record:");
+                        int record = scanner.nextInt() - 1;
+                        phoneBook.editRecord(record);
+                    }
+                    break;
                 case "count":
                     System.out.println("The Phone Book has " + phoneBook.countRecords() + " records.");
                     break;
@@ -88,18 +59,6 @@ public class Main {
         } while (exit == false);
 
 
-
-
-
-//        System.out.println("Enter the name of the person:");
-//        String name = scanner.next();
-//        System.out.println("Enter the surname of the person:");
-//        String surname = scanner.next();
-//        System.out.println("Enter the number:");
-//        String phoneNumber = scanner.next();
-//
-//        Contact contact = new Contact(name, surname, phoneNumber);
-//        System.out.println("A record created!\n" + "A Phone Book with a single record created!");
     }
 
 }
--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PhoneBook {
 
     private ArrayList<Contact> records = new ArrayList<Contact>();
+    Scanner scanner = new Scanner(System.in);
 
 
     public void addRecord(Contact contact) {
@@ -22,6 +24,33 @@ public class PhoneBook {
     public void removeRecords(int index) {
         records.remove(index);
         System.out.println("The record removed!");
+    }
+
+    public void editRecord(int index) {
+        System.out.println("Select a field (name, surname, number):");
+        String field = scanner.next();
+        switch (field) {
+            case "name":
+                System.out.println("Enter the name:");
+                String newName = scanner.next();
+                records.get(index).setName(newName);
+                System.out.println("The record updated!");
+                break;
+            case "surname":
+                System.out.println("Enter the surname:");
+                String newSurname = scanner.next();
+                records.get(index).setSurname(newSurname);
+                System.out.println("The record updated!");
+                break;
+            case "number":
+                System.out.println("Enter the number:");
+                String newNumber = scanner.next();
+                records.get(index).setPhoneNumber(newNumber);
+                System.out.println("The record updated!");
+                break;
+            default:
+                System.out.println("No valid option");
+        }
     }
 
 }
