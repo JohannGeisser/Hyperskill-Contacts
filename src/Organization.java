985129dad20e8803cc6435222a9978c0b37691e4
Johann <johann.geisser.aguilar@gmail.com>
2022-07-30 17:28:09 +0200

    Adding Inheritance mechanism so P&O inherit from Contact class. Corresponding constructors and G&S where also added

--- src/Organization.java
+++ src/Organization.java
@@ -1,5 +1,17 @@
-public class Organization {
+public class Organization extends Contact {
 
     private String address;
 
+    public Organization(String name, String phoneNumber, String address) {
+        super(name, phoneNumber);
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,37 @@
-public class Person {
+public class Person extends Contact {
 
     private String surname;
     private String birthDate;
     private String gender;
 
+    public Person(String name, String phoneNumber, String surname, String birthDate, String gender) {
+        super(name, phoneNumber);
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.gender = gender;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
